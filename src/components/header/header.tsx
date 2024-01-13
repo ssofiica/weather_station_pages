6d9e5cdd4f6c7312615307b718dc7e7806193814
ssofiica <s.valova2016@yandex.ru>
2024-01-14 00:34:24 +0300

    i'am tired

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import './header.css'
 
@@ -11,8 +12,8 @@ function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/weather_station_pages/phenomens">Природные явления</Nav.Link>
-            <Nav.Link href="#link">О станции</Nav.Link>
+            <Link className="link" to="/weather_station_pages/phenomens">Природные явления</Link>
+            <Nav.Link className="link" href="#link">О станции</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
